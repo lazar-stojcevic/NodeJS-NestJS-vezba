0ebe9df1af1b550157546cd67ae9e8c5a0eb6416
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2021-07-27 20:18:10 +0200

    Inplement global api response mechanism

--- src/controlers/api/administrator.controler.ts
+++ src/controlers/api/administrator.controler.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { Administrator } from "entities/administrator.entity";
+import { ApiResponse } from "entities/misc/api.response.class";
 import { AddAdministratorDTO } from "src/dtos/administrator/add.administrator.dto";
 import { EditAdministratorDTO } from "src/dtos/administrator/edit.administrator.dto";
 import { AdministratorService } from "src/services/administrator/administrator.service";
@@ -14,17 +15,17 @@ export class AdministratorControler{
     }
 
     @Get(':id')
-        getById( @Param('id') administratorId: number):Promise<Administrator>{
+        getById( @Param('id') administratorId: number):Promise<Administrator | ApiResponse>{
         return this.administratorService.getById(administratorId);
     }
 
     @Put()
-    add(@Body() data: AddAdministratorDTO): Promise<Administrator>{
+    add(@Body() data: AddAdministratorDTO): Promise<Administrator | ApiResponse>{
         return this.administratorService.add(data);
     }
 
     @Post(':id')
-    edit(@Param('id') id : number, @Body() data: EditAdministratorDTO): Promise<Administrator>{
+    edit(@Param('id') id : number, @Body() data: EditAdministratorDTO): Promise<Administrator| ApiResponse>{
         return this.administratorService.editById(id, data);
     }
 
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
+import { ApiResponse } from 'entities/misc/api.response.class';
+import { response } from 'express';
+import { resolve } from 'path/posix';
 import { AddAdministratorDTO } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDTO } from 'src/dtos/administrator/edit.administrator.dto';
 import { Repository } from 'typeorm'; 
@@ -15,32 +18,52 @@ export class AdministratorService {
         return this.administrator.find();
     }
 
-    getById(id : number): Promise<Administrator>{
-        return this.administrator.findOne(id);
+    getById(id : number): Promise<Administrator | ApiResponse>{
+        return new Promise(async (resolve) =>{
+        let admin = await this.administrator.findOne(id);
+        if (admin === undefined){
+            resolve(new ApiResponse('error', -1002));
+        }
+        resolve(admin);
+        });
+        
     }
 
-    add(data: AddAdministratorDTO): Promise<Administrator>{
+    add(data: AddAdministratorDTO): Promise<Administrator | ApiResponse>{
         //DTO -> model
         //username -> username
         //password -[magija]-> password_Hash
         const crypto = require('crypto');
-
-        const passwordHash = crypto.createHast('sha512');
+        const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
-
         const passwordHastString = passwordHash.digest('hex').toUpperCase();
 
         let newAdmin: Administrator = new Administrator();
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHastString;
-        return this.administrator.save(newAdmin);
+
+
+        return new Promise((resolve) => {
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            })
+        });
     }
 
-    async editById(id: number, data: EditAdministratorDTO):Promise<Administrator>{
+    async editById(id: number, data: EditAdministratorDTO):Promise<Administrator| ApiResponse>{
         let admin: Administrator = await this.administrator.findOne(id);
+
+        if (admin === undefined){
+            return new Promise((resolve) => {
+                resolve(new ApiResponse('error', -1002));
+            });
+        }
         
         const crypto = require('crypto');
-        const passwordHash = crypto.createHast('sha512');
+        const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
         const passwordHastString = passwordHash.digest('hex').toUpperCase();
         
