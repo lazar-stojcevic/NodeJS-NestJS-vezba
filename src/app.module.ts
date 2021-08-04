d6cc8a253233311dfe88a1e0550333794c634d38
lazar-stojcevic <stojcevic.ra84.2018@uns.ac.rs>
2021-08-04 16:13:18 +0200

    Implement the photo upload mehtod

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { AuthMiddleware } from './middlewares/auth.middleware';
 import { AdministratorService } from './services/administrator/administrator.service';
 import { ArticleService } from './services/article/article.service';
 import { CategoryService } from './services/category/category.service';
+import { PhotoService } from './services/photo/photo.service';
 
 
 @Module({
@@ -46,10 +47,26 @@ import { CategoryService } from './services/category/category.service';
         User
       ]
     }),
-    TypeOrmModule.forFeature([Administrator, Category, Article, ArticlePrice, ArticleFeature])
+    TypeOrmModule.forFeature([
+      Administrator,
+        ArticleFeature,
+        ArticlePrice,
+        Article,
+        CartArticle,
+        Cart,
+        Category,
+        Feature,
+        Order,
+        Photo,
+        User])
   ],
   controllers: [AppController, AdministratorControler, CategoryControler, ArticleControler, AuthController],
-  providers: [AdministratorService, CategoryService, ArticleService],
+  providers: [
+    AdministratorService,
+    CategoryService,
+    ArticleService,
+    PhotoService
+  ],
   exports:[ //Ovo radimo da bi nam ove stvari bile dostupne u middleware
     AdministratorService
   ]
--- src/controlers/api/article.controler.ts
+++ src/controlers/api/article.controler.ts
@@ -1,9 +1,15 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 import { Crud } from "@nestjsx/crud";
 import { ArticleFeature } from "entities/article-feature.entity";
 import { Article } from "entities/article.entity";
 import { AddArticleDTO } from "src/dtos/article/add.article.dto";
 import { ArticleService } from "src/services/article/article.service";
+import {diskStorage} from "multer";
+import { StorageConfig } from "config/storage.config";
+import { PhotoService } from "src/services/photo/photo.service";
+import { Photo } from "entities/photo.entity";
+import { ApiResponse } from "entities/misc/api.response.class";
 
 @Controller('api/article')
 @Crud({
@@ -38,10 +44,76 @@ import { ArticleService } from "src/services/article/article.service";
     }
 })
 export class ArticleControler{
-    constructor(public service: ArticleService){}
+    constructor(
+        public service: ArticleService,
+        public photoService: PhotoService
+        ){}
 
     @Post('createFull')
     createFullArticle(@Body() data: AddArticleDTO){
         return this.service.createFullArticle(data);
+    }
+
+    @Post(':id/uploadPhoto/')
+    @UseInterceptors(FileInterceptor('photo', {
+        storage: diskStorage({
+            destination: StorageConfig.photoDestination,
+            filename: (req, file, callback) => {
+                let original: string = file.originalname;
+
+                let normalized = original.replace(/\s+/g, '-');
+
+                normalized = normalized.replace(/[^A-z0-9\.\-]/g, '');
+                let sada = new Date();
+                let datePart = '';
+                datePart += sada.getFullYear().toString();
+                datePart += (sada.getMonth() + 1).toString();
+                datePart += sada.getDate().toString();
+
+                let randomPart : string = new Array(10)
+                    .fill(0)
+                    .map(e => (Math.random() * 10).toFixed(0).toString())
+                    .join('').toString();
+
+                let fileName = datePart + '-' + randomPart + '-' + normalized;
+
+                fileName = fileName.toLowerCase();
+                
+                callback(null, fileName);
+            }
+        }),
+        fileFilter: (req, file, callback) => {
+            if (!file.originalname.toLowerCase().match(/\.(jpg|png)$/)){
+                callback(new Error('Bad file extensions'), false);
+                return;
+            }
+
+            if (!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))){
+                callback(new Error('Bad file content'), false);
+                return; 
+            }
+
+            callback(null, true);
+        },
+        limits: {
+            files: 1,
+            fieldSize: StorageConfig.photoMaxFileSize
+        }
+    })
+    )
+    async uploadPhoto(@Param('id') articleId: number, @UploadedFile() photo):Promise <ApiResponse|Photo>{
+        let filename = photo.filename;
+
+        const newPhoto: Photo = new Photo();
+        newPhoto.articleId = articleId;
+        newPhoto.imagePath = photo.filename;
+        
+        const savedPhoto = await this.photoService.add(newPhoto);
+
+        if(!savedPhoto){
+            return new ApiResponse('error', -4001);
+        }
+
+        return savedPhoto;
     }
 }
